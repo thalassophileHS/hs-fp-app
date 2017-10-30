9bd1e1ab5b8e73e171b5ab9bec3cccf8348f9a50
Catalina Sagan <32839444+thalassophileHS@users.noreply.github.com>
2017-10-30 14:31:21 +0100

    final

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,9 +1,3 @@
 def process_user_query(query_string):
-    x = input().split()
-    for i in x:
-        result = f'Hi, {i}'
-        return result
-
-#x = input().split()
-#for i in x:
-#    print(process_user_query(i), end = '\n')
+    greetings = [f'Hi {i}' for i in query_string.split()]
+    return greetings
