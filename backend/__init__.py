8b19b5d74974757cb8650e83d2b4daf6f2648c97
Catalina Sagan <32839444+thalassophileHS@users.noreply.github.com>
2017-10-30 14:21:48 +0100

    good

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,10 +1,7 @@
 def process_user_query(query_string):
-    student_name = query_string
-    x = []
-    for i in student_name:
-        x += f'Hi, {i}'
-    result = x
+    result = f'Hi, {query_string}'
     return result
 
-
-print(str(process_user_query(input().split())), end = '\n')
+x = input().split()
+for i in x:
+    print(process_user_query(i), end = '\n')
