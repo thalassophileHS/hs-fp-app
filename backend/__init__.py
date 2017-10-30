c6106396ca71af8cde576b6e924264ab713f7d76
Catalina Sagan <32839444+thalassophileHS@users.noreply.github.com>
2017-10-30 13:54:00 +0100

    1

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,3 +1,7 @@
+def print_greeting(student_name):
+    print(f'Hello {student_name}!')
+
 def process_user_query(query_string):
-    result = 'Hi There'
+    x = input().split()
+    result = print_greeting(x)
     return result
