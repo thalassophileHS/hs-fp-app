f5fe04c1a7a7e7dbb5a35348973f040dd4bdec7a
Catalina Sagan <32839444+thalassophileHS@users.noreply.github.com>
2017-11-01 14:21:45 +0100

    1st try dic

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,9 +1,12 @@
 def process_user_query(query_string):
-    s = ['А','Б','В','Г','Д','Е','Ё','Ж','З','И','Й','К','Л','М','Н','О','П','Р','С','Т','У','Ф','Х','Ц','Ч','Ш','Щ','Ъ','Ы','Ь','Э','Ю','Я']
+    lenguages = {
+        'Russian' : ['А','Б','В','Г','Д','Е','Ё','Ж','З','И','Й','К','Л','М','Н','О','П','Р','С','Т','У','Ф','Х','Ц','Ч','Ш','Щ','Ъ','Ы','Ь','Э','Ю','Я']
+    }
+
     for i in query_string.split():
         #for i in t:
-        if i in s:
-            m = 'Russian'
+        if i in lenguages.values():
+            m = lenguages.keys()
             break
         else:
             m = 'No lenguage detected'
