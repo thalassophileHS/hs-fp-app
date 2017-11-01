3912de2fc35131044edc4978c031ec35636291b6
Catalina Sagan <32839444+thalassophileHS@users.noreply.github.com>
2017-11-01 13:32:43 +0100

    Russian first

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,8 +1,6 @@
 def process_user_query(query_string):
-    m = []
-    for i in query_string.split():
-        for t in i:
-            if t.isupper():
-                m.append(f'Hi {i}')
-                break
+    for i in query_string:
+        if (i == й) or (i == ъ) or (i == ы) or (i == э) or (i == щ):
+            m = Russian
+            break
     return m
