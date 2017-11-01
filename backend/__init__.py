c0bd08e7e8a5b0523decbf7d3057019b4c3a36b6
Catalina Sagan <32839444+thalassophileHS@users.noreply.github.com>
2017-11-01 13:36:38 +0100

    added else

--- backend/__init__.py
+++ backend/__init__.py
@@ -3,4 +3,6 @@ def process_user_query(query_string):
         if (i == й) or (i == ъ) or (i == ы) or (i == э) or (i == щ):
             m = Russian
             break
+        else:
+            m = No lenguage detected
     return m
