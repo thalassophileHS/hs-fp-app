3f2317a26f162c219168a6da3caaca0545154cf3
Catalina Sagan <32839444+thalassophileHS@users.noreply.github.com>
2017-11-01 15:23:52 +0100

    Work

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,13 +1,18 @@
 def process_user_query(query_string):
     lenguages = {
-        'Russian' : ['А','Б','В','Г','Д','Е','Ё','Ж','З','И','Й','К','Л','М','Н','О','П','Р','С','Т','У','Ф','Х','Ц','Ч','Ш','Щ','Ъ','Ы','Ь','Э','Ю','Я']
+        'Russian' : 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдеёжзийклмнопрстуфхцчшщъыьэюя'
     }
 
-    for i in query_string.split():
-        #for i in t:
-        if i in lenguages.values():
-            m = lenguages.keys()
-            break
-        else:
-            m = 'No lenguage detected'
-    return m
+    for t in set(query_string.split()):
+        for i in t:
+            for s in lenguages.values():
+                for y in s:
+                    if i == y:
+                        m = lenguages.keys()
+                        m = ' '.join(m)
+                        break
+                    else:
+                        m = 'No lenguage detected'
+    return str(m)
+
+print(process_user_query('В ЮАР Нельсон Мандела также известен как Мадиба (одно из клановых имён народа коса)[26]. Самый пожилой и долгоживущий президент ЮАР: прожил 95 лет (на момент начала президентства — 76 лет, на момент окончания — 81).'))
