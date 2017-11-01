184fcba7fdbba00fef7af2a4b1b2dbd37f02b244
Catalina Sagan <32839444+thalassophileHS@users.noreply.github.com>
2017-11-01 14:15:56 +0100

    change

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,9 +1,10 @@
 def process_user_query(query_string):
-    for x in query_string.split():
-        for i in x:
-            if (i == 'й') or (i == 'ъ') or (i == 'ы') or (i == 'э') or (i == 'щ'):
-                m = 'Russian'
-                break
-            else:
-                m = 'No lenguage detected'
+    s = ['А','Б','В','Г','Д','Е','Ё','Ж','З','И','Й','К','Л','М','Н','О','П','Р','С','Т','У','Ф','Х','Ц','Ч','Ш','Щ','Ъ','Ы','Ь','Э','Ю','Я']
+    for i in query_string.split():
+        #for i in t:
+        if (i == 'й') or (i == 'ь') or (i == 'ы') or (i == 'э') or (i == 'щ'):
+            m = 'Russian'
+            break
+        else:
+            m = 'No lenguage detected'
     return m
