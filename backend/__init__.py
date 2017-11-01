d7551f44ff2e2279f5ae05969e5180b2255da6a9
Catalina Sagan <32839444+thalassophileHS@users.noreply.github.com>
2017-11-01 14:17:45 +0100

    again

--- backend/__init__.py
+++ backend/__init__.py
@@ -2,7 +2,7 @@ def process_user_query(query_string):
     s = ['А','Б','В','Г','Д','Е','Ё','Ж','З','И','Й','К','Л','М','Н','О','П','Р','С','Т','У','Ф','Х','Ц','Ч','Ш','Щ','Ъ','Ы','Ь','Э','Ю','Я']
     for i in query_string.split():
         #for i in t:
-        if (i == 'й') or (i == 'ь') or (i == 'ы') or (i == 'э') or (i == 'щ'):
+        if i in s:
             m = 'Russian'
             break
         else:
