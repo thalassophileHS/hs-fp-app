7937392a08a76bb2f752f84eb5aa1362ba621c18
Catalina Sagan <32839444+thalassophileHS@users.noreply.github.com>
2017-11-01 13:53:51 +0100

    Hope it works

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,8 +1,9 @@
 def process_user_query(query_string):
-    for i in query_string:
-        if (i == й) or (i == ъ) or (i == ы) or (i == э) or (i == щ):
-            m = 'Russian'
-            break
-        else:
-            m = 'No lenguage detected'
+    for x in query_string.split():
+        for i in x:
+            if (i == 'й') or (i == 'ъ') or (i == 'ы') or (i == 'э') or (i == 'щ'):
+                m = 'Russian'
+                break
+            else:
+                m = 'No lenguage detected'
     return m
