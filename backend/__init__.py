d02e96c4fa9d6bc313242723765829e701454571
Catalina Sagan <32839444+thalassophileHS@users.noreply.github.com>
2017-11-01 13:39:54 +0100

    Dumb error solved

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,8 +1,8 @@
 def process_user_query(query_string):
     for i in query_string:
         if (i == й) or (i == ъ) or (i == ы) or (i == э) or (i == щ):
-            m = Russian
+            m = 'Russian'
             break
         else:
-            m = No lenguage detected
+            m = 'No lenguage detected'
     return m
