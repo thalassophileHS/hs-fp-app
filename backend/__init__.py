12df232c692805ae5abf66d3b5a3f42addd594bc
Catalina Sagan <32839444+thalassophileHS@users.noreply.github.com>
2017-11-02 11:21:51 +0100

    One

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,26 +1,27 @@
 def process_user_query(query_string):
     lenguages = {
-        'Slavic' : 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдеёжзийклмнопрстуфхцчшщъыьэюя',
+        'Slavic' : 'БГДЁЖЗИЙЛФЦЧШЩЪЫЬЭЮЯ',
         'Armenian' : 'ԱԲԳԴԵԶԷԸԹԺԻԼԽԾԿՀՁՂՃՄՅՆՇՈՉՊՋՌՍՎՏՐՑՒՓՔՕՖ',
         'Georgian' : 'აბგდევზჱთიკლმნჲოპჟრსტჳუფქღყშჩცძწჭხჴჯჰჵჶჷჸ',
-        'Latin' : 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+        'Latin' : 'DFGJLQRSTVWZ',
         'Chinese' : '诶比西迪伊弗吉尺杰开勒马娜哦屁吉吾儿艾丝提伊吾维豆贝尔维克斯吾贼德',
         'Japanese' : 'あいうえおかきくけこがぎぐげごさしすせそざじずぜぞたちつてとだぢづでどなにぬねのはひふへほばびぶべぼぱぴぷぺぽまみむめもやゆよらりるれろわをん',
         'Korean' : 'ㅂㄷㅈㄱㅃㄸㅉㄲㅍㅌㅊㅋㅅㅎㅆㅁㄴㅇㄹㅣㅔㅚㅐㅏㅗㅜㅓㅡㅢㅖㅒㅑㅛㅠㅕㅟㅞㅙㅘㅝ',
         'Cambodian' : 'កខគឃងចឆជឈញដឋឌឍណតថទធនបផពភមយរលវឝឞសហឡអ',
-        'Greek' : 'ΑαΒβΓγΔδΕεΖζΗηΘθΙιΚκΛλΜμΝΞξΟοΠπΡρΣΤτΥυΦφΧχΨψΩω',
+        'Greek' : 'αβΓγΔδεΖζηΘθιΛλμΞξπρΣΤτΥΦφχΨψΩω',
         'Arabic' : 'غظ ض ذ خ ث ت ش ر ق ص ف ع س ن م ل ك ي ط ح ز و ه د ج ب ا'
     }
 
-    for t in set(query_string.split()):
-        for i in t:
-            for q,s in lenguages.items():
-                for y in s:
-                    if i == y:
-                        m = q
+    for word in set(query_string.split()):
+        for letter in word:
+            for key,value in lenguages.items():
+                for char in value:
+                    if letter == char:
+                        m = key
                         break
                     else:
                         m = 'No lenguage detected'
+                        continue
     return str(m)
 
-#print(process_user_query('نلسون رولیهلاهلا ماندلا (وه دی اومائه د ۲۶ تیر ۱۲۹۷ همبایی وا ۱۸ جوئیه ۱۹۱۸ - د دنیا رئته د ۱۴ آذر ۱۳۹۲ همبایی وا ۵ دسامر ۲۰۱۳) اولین سرکومره ولات افریقا هارگه ئه که د انتخاوات خلک سالار خلکمن انتخاوه بی'))
+print(process_user_query('JAUHFG'))
