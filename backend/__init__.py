58a79e89fc7c7debb9c0e473416d76b8c9ddf7e2
Catalina Sagan <32839444+thalassophileHS@users.noreply.github.com>
2017-11-02 14:56:12 +0100

    Final 1

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,5 +1,5 @@
 def process_user_query(query_string):
-    lenguages = {
+    lenguages_char = {
         'Slavic' : 'БГДЁЖЗИЙЛФЦЧШЩЪЫЬЭЮЯ',
         'Armenian' : 'ԱԲԳԴԵԶԷԸԹԺԻԼԽԾԿՀՁՂՃՄՅՆՇՈՉՊՋՌՍՎՏՐՑՒՓՔՕՖ',
         'Georgian' : 'აბგდევზჱთიკლმნჲოპჟრსტჳუფქღყშჩცძწჭხჴჯჰჵჶჷჸ',
@@ -9,19 +9,54 @@ def process_user_query(query_string):
         'Korean' : 'ㅂㄷㅈㄱㅃㄸㅉㄲㅍㅌㅊㅋㅅㅎㅆㅁㄴㅇㄹㅣㅔㅚㅐㅏㅗㅜㅓㅡㅢㅖㅒㅑㅛㅠㅕㅟㅞㅙㅘㅝ',
         'Cambodian' : 'កខគឃងចឆជឈញដឋឌឍណតថទធនបផពភមយរលវឝឞសហឡអ',
         'Greek' : 'αβΓγΔδεΖζηΘθιΛλμΞξπρΣΤτΥΦφχΨψΩω',
-        'Arabic' : 'غظ ض ذ خ ث ت ش ر ق ص ف ع س ن م ل ك ي ط ح ز و ه د ج ب ا'
+        'Arabic': 'غظضذخثتشرقصفعسنملكيطحزوهدجبا',
+        'Tamil' : 'அஆஇஈஉஊஎஏஐஒஓஔகஙசஞடணதநபமய'
     }
-
+    leng = {
+        'French' : ['le', 'il'],
+        'Spanish' : ['los', 'las', 'unos', 'unas', 'y'],
+        'Romanian' : ['și', 'în', 'între', 'cea', 'au'],
+        'Italina' : ['è', 'di', 'con', 'per'],
+        'Portuguese' : [ 'à', 'e', 'é', 'às', 'em', 'são', 'mas', 'que'],
+        'Wallon' : ['dji', 'nén', 'rén', 'bén', 'pol', 'ås', 'vs', 'ki', 'pô', 'èn', 'dj'],
+        'English' : [ 'an', 'and', 'in', 'of', 'the', 'that', 'is', 'I'],
+        'German' : [ 'der', 'die', 'dem', 'sie', 'ist', 'ich', 'aber'],
+        'Dutch' : ['het', 'op', 'en', 'een', 'voor'],
+        'Afrikaans' : [ f'\'n', 'vir', 'nie'],
+        'Swedish' : [ 'och', 'att', 'det', 'som', 'är'],
+        'Danish' : ['af', 'til', 'med', 'det'],
+        'Norwegian' : ['ble', 'å', 'eller'],
+        'Latvian' : ['bija', 'tika', 'viņš'],
+        'Lithuanian' : ['yra', 'kad'],
+        'Polish' : [ 'w', 'jest', 'się'],
+        'Czech' : ['z', 'k', 'až', 'však'],
+        'Slovak' : [ 'yr', 'ac'],
+        'Turkish' : ['bir', 'bu', 'fakat', 'oldu', 'şu'],
+        'Welsh' : ['yr', 'ac'],
+        'Finnish' : ['sinä'],
+        'Estonian' : ['või'],
+        'Hungarian' : ['az', 'ez', 'egy', 'hogy']
+    }
     for word in set(query_string.split()):
         for letter in word:
-            for key,value in lenguages.items():
+            for key,value in lenguages_char.items():
                 for char in value:
                     if letter == char:
                         m = key
-                        break
-                    else:
-                        m = 'No lenguage detected'
-                        continue
-    return str(m)
+                        #return m
+    if m == 'Latin':
+        for word in query_string.split():
+            for key, value in leng.items():
+                if word in value:
+                    m = key
+    if m == 'Slavic':
+        for word in query_string.split():
+            if word == 'със':
+                m = 'Bulgarian'
+            for letter in word:
+                if letter in ['ј', 'љ', 'њ','џ', 'ђ', 'ћ']:
+                    m = 'Serbian'
+                else:
+                    m = 'Russian'
 
-print(process_user_query('JAUHFG'))
+    return m
